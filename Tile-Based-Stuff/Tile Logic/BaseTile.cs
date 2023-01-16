5fee837768b9ec957481a783bd22dc6ae712e49f
Joseph Rousoss <jroussos01@gmail.com>
2023-01-16 15:50:46 -0500

    added some comments to the tile classes

--- Tile-Based-Stuff/Tile Logic/BaseTile.cs
+++ Tile-Based-Stuff/Tile Logic/BaseTile.cs
@@ -3,6 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// How to use this class:
+/*
+ * This is an abstract class that Walkable, UnWalkable, and possibly other tiles inherit from
+ * Just make sure this script is present in your project and that's all you need to do.
+ */
 public abstract class BaseTile : MonoBehaviour
 {
     private Vector2Int location;
--- Tile-Based-Stuff/Tile Logic/WalkableTile.cs
+++ Tile-Based-Stuff/Tile Logic/WalkableTile.cs
@@ -3,6 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// How to use this class:
+/*
+ * Make a prefab called "WalkableTile" that contains at minimum a sprite renderer and this script.
+ * Make a tilemap grid at (0,0,0) in your scene, and create a tilemap child inside it at (0.5,0.5,0) relative to the grid
+ * Drag and drop one "WalkableTile" into the tilemap child and line it up with the grid.
+ * Use the GameObject brush and the eye-dropper tool to select and paint as many tiles as you want into your scene.
+ */
 public class WalkableTile : BaseTile
 {
     private SpriteRenderer tileSprite;
